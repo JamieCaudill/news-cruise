dc5b826aaa28148318c4fda578636bf67048a4c9
Jamie Caudill <j.caudill7177@gmail.com>
2023-09-19 12:36:36 -0700

    feat: Setup modular components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <h1 className="Header__title">News Cruise</h1>
+    </header>
+  );
+};
+
+export default Header;
