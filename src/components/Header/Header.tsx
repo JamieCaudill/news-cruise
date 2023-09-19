1da7f107ed523b4f9729c0d614347d8a456e7f8c
Jamie Caudill <j.caudill7177@gmail.com>
2023-09-19 13:06:51 -0700

    feat: Display header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <header className="Header">
-      <h1 className="Header__title">News Cruise</h1>
+    <header className="header">
+      <h1 className="header__title">News Cruise</h1>
     </header>
   );
 };
