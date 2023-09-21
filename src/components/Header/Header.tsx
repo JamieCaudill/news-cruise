3a5fc15e9fc374b453b331d2cc736737cc7ca72a
Jamie Caudill <j.caudill7177@gmail.com>
2023-09-21 10:23:56 -0700

    feat: Change backgroudcolor

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <Link to="/"><h1 className="header__title">News Cruise</h1></Link>
+      <h3 className="header__subtitle">Today's Top Stories in Tech</h3>
     </header>
   );
 };
