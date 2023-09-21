5ba2dbf37cfa46b47b265b207b6bd2101004090a
Jamie Caudill <j.caudill7177@gmail.com>
2023-09-21 10:58:20 -0700

    feat: Update tests for error handling

--- src/apiCalls.ts
+++ src/apiCalls.ts
@@ -1,7 +1,7 @@
 const getArticles = async () => {
   try {
     const response = await fetch(
-      "https://newssapi.org/v2/top-headlines?country=us&apiKey=180f2a4ded404054a9c41418048e9004&category=technology"
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=180f2a4ded404054a9c41418048e9004&category=technology"
     );
     if (!response.ok) {
       throw new Error("Failed to fetch data");
@@ -10,7 +10,6 @@ const getArticles = async () => {
     const data = await response.json();
     return data;
   } catch (error) {
-    console.log("error", error);
     throw error;
   }
 };
